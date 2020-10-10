eab2b49f6493be0bbbd98e9c75d7cd12a999438c
doggy <doggybay12@gmail.com>
2020-10-10 01:05:24 +0000

    adding material ui link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from '../src/Link';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 export default function Home() {
   return (
@@ -51,14 +55,17 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <Container maxWidth="sm">
+          <Box my={4}>
+            <Typography variant="h4" component="h1" gutterBottom>
+              Next.js example
+        </Typography>
+            <Link href="/about" color="secondary">
+              Go to the about page
+        </Link>
+            
+          </Box>
+        </Container>
       </footer>
     </div>
   )
